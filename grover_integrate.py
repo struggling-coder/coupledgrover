aeaf809c65be6f1b21e2621c37fc9b8d37ab9956
arkyaC <arkya.chat@gmail.com>
2017-04-09 18:39:26 +0530

    animation done 1.0

--- grover_integrate.py
+++ grover_integrate.py
@@ -5,6 +5,7 @@ import scipy.integrate as integrate
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import pygame
+import time
 
 class Mass:
     def __init__(self,(x,y),radius):
@@ -49,8 +50,8 @@ def solver():
 
 #physical parameters
 M = 64.0;m = 15.0;k = 5.0;K = 12.0
-N = 100 #number of m's
-V = 1.0 #init vel of m's
+N = 50 #number of m's
+V = 20.0 #init vel of m's
 wt = np.sqrt(k/m) #$\omega_{t}$
 wp = 1.5*wt #$\omega_{+}$
 wm = 0.5*wt #$\omega_{-}$
@@ -58,43 +59,53 @@ wm = 0.5*wt #$\omega_{-}$
 #Q = int(np.pi/4 * np.sqrt(N)) #theoretical limit of no. of iterations
 target = 3 #target entry out of [0,1,2,3,...]
 delta = 2*np.pi/wt #time period of tapping = half the time period of target
-Q = int( np.sqrt(N)) #theoretical limit of no. of iterations
+Q = int(np.pi/4 * np.sqrt(N)) #theoretical optimal limit of no. of iterations
 t_end = delta*Q #corresponding time limit
-res = 20 #resolution
+res = 50 #resolution
 dt = delta/res #each half cycle is divided with a resolution of res
 
-solution = solver()
-#print solution[res*Q-1] #test
+solution = solver() #solving the ode
 
+#velocity space graph
+'''
 t = np.arange(0,dt*len(solution),dt)
 plt.plot(t,(solution[:,target+N]),'r--')
 plt.plot(t,(solution[:,target+N-1]),'b--')
 plt.xlabel("t")
 plt.ylabel("velocity")
 plt.show()
-#graphics
 '''
-(width,height) = (800,600)
-rad = 10 #radius of each m
+
+#graphics
+(width,height) = (1000,800)
+rad = 5 #radius of each m
 x0 = width/(N+1)
 y0 = height/2
 
-background_colour = (20,255,200)
+background_colour = (255,255,255)
 screen = pygame.display.set_mode((width, height))
-
 pygame.display.set_caption('Grover\'s Algorithm with Coupled Oscillators')
-screen.fill(background_colour)
+pygame.display.flip() #to display the window
 
 masses = []
 for i in range(N):
     masses.append(Mass((x0+i*x0,y0),rad))
-for mass in masses:
-    mass.display()
 
-pygame.display.flip() #to display the window
+ti = 0.0
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-'''
+    screen.fill(background_colour)
+    for i in range(N):
+        masses[i].y = y0+int(solution[int(ti/dt),i])
+        masses[i].display()
+    print ti
+    pygame.display.flip()
+    clock = pygame.time.Clock()
+    ti += dt
+    if int(ti/dt) == len(solution):
+        ti = 0.0
+        time.sleep(5)
+    clock.tick(10calc)
